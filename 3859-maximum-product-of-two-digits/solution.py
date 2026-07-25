7b21fa9b495cf4367d2222254a48d5cbe7fe2034
Surya Pratap Singh <suryaps.cs@gmail.com>
2026-07-25 15:22:48 +0000

    Sync LeetCode submission Runtime - 1 ms (34.69%), Memory - 19.4 MB (22.54%)

--- 3859-maximum-product-of-two-digits/solution.py
+++ 3859-maximum-product-of-two-digits/solution.py
@@ -1,4 +1,4 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
-        digits = sorted(int(d) for d in str(n))
-        return digits[-1] * digits[-2]
+        digits = sorted([int(d) for d in str(n)], reverse=True)
+        return digits[0] * digits[1]
